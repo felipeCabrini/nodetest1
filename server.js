d0af4b0698f8c918d73ec99ae19e50280844cceb
unknown <felipehurri@gmail.com>
2017-05-13 00:25:40 -0300

    ss

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-  var connect = require("connect"); connect().use(connect.static(__dirname)).listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+  var connect = require("connect"); connect().use(connect.static(__dirname)).listen(server_port);
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+connect().use(serveStatic(__dirname)).listen(server_port, function(){
+    console.log('Server running on' + server_port);
 
 });
